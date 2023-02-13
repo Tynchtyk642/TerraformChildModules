af2d944655ac7d9bfd92d453e98b7e2b87627d4f
Nurbolot <you@example.com>
2023-02-13 13:58:29 +0600

    final-project

---
+++ eks/variable.tf
@@ -0,0 +1,35 @@
+variable "vpc_id" {
+  type = string 
+}
+
+variable "subnet_public_id" {
+  type = list (string)
+}
+
+variable "subnet_private_id" {
+  type = list (string)
+}
+
+variable "project" {
+  description = "Name to be used on all the resources as identifier. e.g. Project name, Application name"
+  # description = "Name of the project deployment."
+  type = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_bits" {
+  description = "The number of subnet bits for the CIDR. For example, specifying a value 8 for this parameter will create a CIDR with a mask of /24."
+  type        = number
+  default     = 8
+}
+
+
+variable "instance_type" {
+  type = string 
+  default = "t2.micro"
+}
---
+++ vpc/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+
+output "subnet_public_id" {
+  value = aws_subnet.public.*.id
+}
+
+output "subnet_private_id" {
+  value = aws_subnet.private.*.id
+}
+
+ 
+  
